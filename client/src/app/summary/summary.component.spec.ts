d2899a34ed1d6722e35ed5d5d2b0772d1daa762c
twarling <warli010@morris.umn.edu>
2018-03-01 11:40:58 -0600

    Fix error in summary.component.spec #4

--- client/src/app/summary/summary.component.spec.ts
+++ client/src/app/summary/summary.component.spec.ts
@@ -1,25 +1,25 @@
 import {TestBed, ComponentFixture} from '@angular/core/testing';
-import {HomeComponent} from './summary.component';
+import {SummaryComponent} from './summary.component';
 import {DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
 import {CustomModule} from '../custom.module';
 import {MATERIAL_COMPATIBILITY_MODE} from '@angular/material';
 
-describe('Home', () => {
+describe('Summary', () => {
 
-    let component: HomeComponent;
-    let fixture: ComponentFixture<HomeComponent>;
+    let component: SummaryComponent;
+    let fixture: ComponentFixture<SummaryComponent>;
     let de: DebugElement;
     let el: HTMLElement;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [CustomModule],
-            declarations: [HomeComponent], // declare the test component
+            declarations: [SummaryComponent], // declare the test component
             providers: [{provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}],
         });
 
-        fixture = TestBed.createComponent(HomeComponent);
+        fixture = TestBed.createComponent(SummaryComponent);
 
         component = fixture.componentInstance; // BannerComponent test instance
 
