481b348deac3529f826e0c6a7725c2aec2a6852f
HuntsmanRoym <grins008@morris.umn.edu>
2018-03-03 22:49:29 -0600

    Start E2E testing
    - Create setup for E2E testing the homepage

---
+++ client/e2e/home.po.ts
@@ -0,0 +1,39 @@
+import {browser, element, by, promise} from 'protractor';
+import {Key} from 'selenium-webdriver';
+
+export class homePage {
+    static navigateTo(): promise.Promise<any> {
+        return browser.get('/');
+    }
+
+    static backspace() {
+        browser.actions().sendKeys(Key.BACK_SPACE).perform();
+    }
+
+    }
+
+    // http://www.assertselenium.com/protractor/highlight-elements-during-your-protractor-test-run/
+  /*  highlightElement(byObject) {
+        function setStyle(element, style) {
+            const previous = element.getAttribute('style');
+            element.setAttribute('style', style);
+            setTimeout(() => {
+                element.setAttribute('style', previous);
+            }, 200);
+            return 'highlighted';
+        }
+
+        return browser.executeScript(setStyle, element(byObject).getWebElement(), 'color: red; background-color: yellow;');
+    }
+*/
+  /*
+     buttonExists(): promise.Promise<boolean> {
+        this.highlightElement(by.id('selectEmoji'));
+        return element(by.id('selectEmoji')).isPresent();
+    }
+
+    clickSelectEmojiButton(): promise.Promise<void> {
+        this.highlightElement(by.id('selectEmoji'));
+        return element(by.id('selectEmoji')).click();
+    }
+*/
