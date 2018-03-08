3025b5be6bbcafcf1e6f44df0be51e0222519a48
twarling <warli010@morris.umn.edu>
2018-03-08 00:42:56 -0600

    Re-implement filter by date functionality

--- client/src/app/summary/summary-list.component.ts
+++ client/src/app/summary/summary-list.component.ts
@@ -18,43 +18,14 @@ export class SummaryListComponent implements OnInit {
     startDate;
     endDate;
     getDate;
-    private setMood: string;
 
-    private morning = 0;
-    private afternoon = 0;
-    private night = 0;
-
-    private oneAm = 0;
-    private twoAm = 0;
-    private threeAm = 0;
-    private fourAm = 0;
-    private fiveAm = 0;
-    private sixAm = 0;
-    private sevenAm = 0;
-    private eightAm = 0;
-    private nineAm = 0;
-    private tenAm = 0;
-    private elevenAm = 0;
-    private twelvePm = 0;
-    private onePm = 0;
-    private twoPm = 0;
-    private threePm = 0;
-    private fourPm = 0;
-    private fivePm = 0;
-    private sixPm = 0;
-    private sevenPm = 0;
-    private eightPm = 0;
-    private ninePm = 0;
-    private tenPm = 0;
-    private elevenPm = 0;
-    private twelveAm = 0;
     // These are public so that tests can reference them (.spec.ts)
     public summarys: Summary[];
     public filteredSummarys: Summary[];
 
     // These are the target values used in searching.
     // We should rename them to make that clearer.
-    
+
     public summaryMood: string;
 
     // The ID of the
@@ -69,7 +40,7 @@ export class SummaryListComponent implements OnInit {
         return summary._id['$oid'] === this.highlightedID['$oid'];
     }
 
-    public filterSummarys(searchMood: string): Summary[] {
+    public filterSummarys(searchMood: string, searchStartDate: any, searchEndDate: any): Summary[] {
 
         this.filteredSummarys = this.summarys;
 
@@ -79,6 +50,24 @@ export class SummaryListComponent implements OnInit {
 
             this.filteredSummarys = this.filteredSummarys.filter(summary => {
                 return !searchMood || summary.mood.toLowerCase().indexOf(searchMood) !== -1;
+            });
+        }
+
+        // Filter by startDate
+        if (searchStartDate != null) {
+
+            this.filteredSummarys = this.filteredSummarys.filter(summary => {
+                this.getDate = new Date(summary.date);
+                return this.getDate >= this.startDate;
+            });
+        }
+
+        // Filter by endDate
+        if (searchEndDate != null) {
+
+            this.filteredSummarys = this.filteredSummarys.filter(summary => {
+                this.getDate = new Date(summary.date);
+                return this.getDate <= this.endDate;
             });
         }
 
@@ -99,7 +88,7 @@ export class SummaryListComponent implements OnInit {
         summaryListObservable.subscribe(
             summarys => {
                 this.summarys = summarys;
-                this.filterSummarys(this.summaryMood);
+                this.filterSummarys(this.summaryMood, this.startDate, this.endDate);
             },
             err => {
                 console.log(err);
@@ -124,128 +113,12 @@ export class SummaryListComponent implements OnInit {
         return this.summarys.length;
     }
 
-    totalNumberMoods(mood: string): number{
-        this.filteredSummarys = this.summarys;
-        this.filteredSummarys = this.filteredSummarys.filter(summary=>{
-            return !mood || mood.toLowerCase().indexOf(summary.mood) !== -1;
-        })
-
-        return this.filteredSummarys.length;
+    totalNumberMoods(): number{
+       return this.filteredSummarys.length;
     }
 
-    aveTime(mood: string, time: number) {
-        this.getDate = new Date(time);
-        var hour = this.getDate.getHours();
-
-        if (mood != this.setMood) {
-            this.resetNums()
-            this.setMood = mood;
-        }
-
-        /*  if(mood != this.setMood){
-              this.resetNums()
-              this.setMood = mood;
-          }
-
-          if(time >= 600 && time <= 1100){
-              this.morning++;
-          }
-          else if(time >= 1101 && time <= 1600){
-              this.afternoon++;
-          }
-          else{
-              this.night++;
-          }*/
-        if(hour >= 0 && hour < 1) {
-            this.twelveAm++;
-        }
-        else if (hour > 1 && hour < 2 ) {
-            this.oneAm++;
-        }
-        else if (hour >= 2 && hour < 3) {
-            this.twoAm++;
-        }
-        else if (hour >= 3 && hour < 4) {
-            this.threeAm++;
-        }
-        else if (hour >= 4 && hour < 5) {
-            this.fourAm++;
-        }
-        else if (hour >= 5 && hour < 6) {
-            this.fiveAm++;
-        }
-        else if (hour >= 6 && hour < 7) {
-            this.sixAm++;
-        }
-        else if (hour >= 7 && hour < 8) {
-            this.sevenAm++;
-        }
-        else if (hour >= 8 && hour < 9) {
-            this.eightAm++;
-        }
-        else if (hour >= 9 && hour < 10) {
-            this.nineAm++;
-        }
-        else if (hour >= 10 && hour < 11) {
-            this.tenAm++;
-        }
-        else if (hour >= 11 && hour < 12) {
-            this.elevenAm++;
-        }
-        else if (hour >= 12 && hour < 13) {
-            this.twelvePm++;
-        }
-        else if (hour >= 13 && hour < 14) {
-            this.onePm++;
-        }
-        else if (hour >= 14 && hour < 15) {
-            this.twoPm++;
-        }
-        else if (hour >= 15 && hour < 16) {
-            this.threePm++;
-        }
-        else if (hour >= 16 && hour < 17) {
-            this.fourPm++;
-        }
-        else if (hour >= 17 && hour < 18) {
-            this.fivePm++;
-        }
-        else if (hour >= 18 && hour < 19) {
-            this.sixPm++;
-        }
-        else if (hour >= 19 && hour < 20) {
-            this.sevenPm++;
-        }
-        else if (hour >= 20 && hour < 21) {
-            this.eightPm++;
-        }
-        else if (hour >= 21 && hour < 22) {
-            this.ninePm++;
-        }
-        else if (hour >= 22 && hour < 23) {
-            this.tenPm++;
-        }
-        else if (hour >= 23 && hour < 24) {
-            this.elevenPm++;
-        }
-    }
-
-
-    returnTime(mood: string): string{
-        if(mood == ""){
-            return "";
-        }
-
-        if(this.morning > this.afternoon && this.morning > this.night){
-            return "You usually feel " + mood + " in the morning";
-        }
-
-        else if (this.afternoon > this.morning && this.afternoon > this.night){
-            return "You usually feel " + mood + " in the afternoon";
-        }
-        else{
-            return "You usually feel " + mood + " in the night";
-        }
+   returnTime(mood: string): string{
+     return "";
     }
 
     openDialog(): void {
@@ -255,75 +128,12 @@ export class SummaryListComponent implements OnInit {
         });
     }
 
-
-
-
-
-    /*getHeight(time:number):number{
-        if(time == 200){
-            return this.twoAm;
-        }
-        if(time == 400){
-            return this.fourAm;
-        }
-        if(time == 600){
-            return this.sixAm;
-        }
-        if(time == 800){
-            return this.eightAm;
-        }
-        if(time == 1000){
-            return this.tenAm;
-        }
-        if(time == 1200){
-            return this.twelvePm;
-        }
-        if(time == 1400){
-            return this.twoPm;
-        }
-        if(time == 1600){
-            return this.fourPm;
-        }
-        if(time == 1800){
-            return this.sixPm;
-        }
-        if(time == 2000){
-            return this.eightPm;
-        }
-        if(time == 2200){
-            return this.tenPm;
-        }
-        if(time == 2400){
-            return this.twelveAm;
-        }
-    }*/
-
-
-
-    resetNums(){
-
-        this.morning = 0;
-        this.afternoon = 0;
-        this.night = 0;
-
-        /*this.twoAm = 0;
-        this.fourAm = 0;
-        this.sixAm = 0;
-        this.eightAm = 0;
-        this.tenAm = 0;
-        this.twelvePm = 0;
-        this.twoPm = 0;
-        this.fourPm = 0;
-        this.sixPm = 0;
-        this.eightPm = 0;
-        this.tenPm = 0;
-        this.twelveAm = 0;*/
-    }
-
     ngOnInit(): void {
         this.refreshSummarys();
         this.loadService();
         this.startDate = new Date();
+        this.startDate.setHours(0,0,0,0)
         this.endDate = new Date();
+        this.endDate.setHours(23,59,59,0)
     }
 }
